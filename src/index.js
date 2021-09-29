ca3e3b3b17916641498e8791485b2cace12099e0
Anderson Godoy <anderson.a.godoy@gmail.com>
2021-09-29 12:16:12 -0300

    Desafio 03 ignite completo

--- src/index.js
+++ src/index.js
@@ -8,9 +8,76 @@ app.use(express.json());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  return response.json(repositories);
-});
+app.get('/repositories', (request, response) => {
+	return response.json(repositories)
+})
+
+app.post('/repositories', (request, response) => {
+	const { title, url, techs } = request.body
+
+	const repository = {
+		id: uuid(),
+		title,
+		url,
+		techs,
+		likes: 0
+	}
+
+	repositories.push(repository)
+
+	return response.status(201).send(repository)
+})
+
+app.put('/repositories/:id', (request, response) => {
+	const { id } = request.params
+	const { title, url, techs } = request.body
+	const updatedRepository = { title, url, techs }
+
+	const repositoryIndex = repositories.findIndex((repository) => repository.id === id)
+
+	if (repositoryIndex < 0) {
+		return response.status(404).json({ error: 'Repository not found' })
+	}
+
+	const repository = { ...repositories[repositoryIndex], ...updatedRepository }
+
+	repositories[repositoryIndex] = repository
+
+	return response.json(repository)
+})
+
+app.delete('/repositories/:id', (request, response) => {
+	const { id } = request.params
+
+	const repositoryIndex = repositories.findIndex((repository) => repository.id === id)
+	const repository = repositories.some((repo) => repo.id === id)
+
+	if (!repository) {
+		return response.status(404).json({ error: 'Repository not found' })
+	}
+
+	repositories.splice(repositoryIndex, 1)
+
+	return response.status(204).send()
+})
+
+app.post('/repositories/:id/like', (request, response) => {
+	const { id } = request.params
+
+	const repositoryIndex = repositories.findIndex((repository) => repository.id === id)
+	const repository = repositories.find((repo) => repo.id === id)
+
+	if (!repository) {
+		return response.status(404).json({ error: 'Repository not found' })
+	}
+
+	++repositories[repositoryIndex].likes
+
+	return response.json(repository)
+})
+
+module.exports = app
+
 
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body
